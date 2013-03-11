de6e8d00f815e30124b7ab233248669728bc88a2
metaphorcreations <metaphorcreations@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-11 17:41:22 +0000

    Fixed update notice
    git-svn-id: https://plugins.svn.wordpress.org/mtphr-shortcodes/trunk@679893 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mtphr-shortcodes.php
+++ mtphr-shortcodes.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Metaphor Shortcodes
 Description: Includes Column Grids, Pricing Tables, Post Sliders & Post Blocks.
-Version: 1.0.0
+Version: 1.0.1
 Author: Metaphor Creations
 Author URI: http://www.metaphorcreations.com
 License: GPL2
@@ -27,52 +27,40 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 
 
+	
+// Check if the old plugin is installed
+$active_plugins = get_option( 'active_plugins', array() );
+if( in_array('mtphr-shortcodes-pack-1/mtphr-shortcodes-pack-1.php', $active_plugins) ) {
 
-add_action( 'admin_init', 'mtphr_shortcodes_widgets_init', 20 );
-add_action( 'init', 'mtphr_shortcodes_widgets_init', 20 );
-/**
- * Make sure to load the plugin a little later than normal
- *
- * @since 1.0.0
- */
-function mtphr_shortcodes_widgets_init() {
-	
-	
-	// Check if the old plugin is installed
-	$active_plugins = get_option( 'active_plugins', array() );
-	if( in_array('mtphr-shortcodes-pack-1/mtphr-shortcodes-pack-1.php', $active_plugins) ) {
-	
-		add_action('admin_notices', 'mtphr_shortcodes_admin_notice');
-		/**
-		 * Display an admin notice
-		 *
-		 * @since 1.0.0
-		 */
-		function mtphr_shortcodes_admin_notice(){
-	    echo '<div class="updated"><p>'.__('In order to use the new <strong>Metaphor Shortcodes</strong> you must deactivate <strong>Metaphor Shortcodes Pack #1</strong>','mtphr-shortcodes').'</p></div>';
-		}
-		
-	} else {
-	
-		/**Define Widget Constants */
-		if ( WP_DEBUG ) {
-			define ( 'MTPHR_SHORTCODES_VERSION', '1.0.0-'.time() );
-		} else {
-			define ( 'MTPHR_SHORTCODES_VERSION', '1.0.0' );
-		}
-		define ( 'MTPHR_SHORTCODES_DIR', plugin_dir_path(__FILE__) );
-		define ( 'MTPHR_SHORTCODES_URL', plugins_url().'/mtphr-shortcodes' );
-		
-		
-		
-		
-		// Load the general functions
-		require_once( MTPHR_SHORTCODES_DIR.'includes/scripts.php' );
-		require_once( MTPHR_SHORTCODES_DIR.'includes/functions.php' );
-		require_once( MTPHR_SHORTCODES_DIR.'includes/shortcodes.php' );
-	
+	add_action('admin_notices', 'mtphr_shortcodes_admin_notice');
+	/**
+	 * Display an admin notice
+	 *
+	 * @since 1.0.0
+	 */
+	function mtphr_shortcodes_admin_notice(){
+    echo '<div class="updated"><p>'.__('In order to use the new <strong>Metaphor Shortcodes</strong> you must deactivate <strong>Metaphor Shortcodes Pack #1</strong>','mtphr-shortcodes').'</p></div>';
 	}
-}
+	
+} else {
 
+	/**Define Widget Constants */
+	if ( WP_DEBUG ) {
+		define ( 'MTPHR_SHORTCODES_VERSION', '1.0.1-'.time() );
+	} else {
+		define ( 'MTPHR_SHORTCODES_VERSION', '1.0.1' );
+	}
+	define ( 'MTPHR_SHORTCODES_DIR', plugin_dir_path(__FILE__) );
+	define ( 'MTPHR_SHORTCODES_URL', plugins_url().'/mtphr-shortcodes' );
+	
+	
+	
+	
+	// Load the general functions
+	require_once( MTPHR_SHORTCODES_DIR.'includes/scripts.php' );
+	require_once( MTPHR_SHORTCODES_DIR.'includes/functions.php' );
+	require_once( MTPHR_SHORTCODES_DIR.'includes/shortcodes.php' );
+
+}
 
 
