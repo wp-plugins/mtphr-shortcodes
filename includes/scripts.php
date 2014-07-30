38793004f29051aae6f1934b5e01d4f05689040f
metaphorcreations <metaphorcreations@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-30 22:34:27 +0000

    multiple updates
    git-svn-id: https://plugins.svn.wordpress.org/mtphr-shortcodes/trunk@957701 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/scripts.php
+++ includes/scripts.php
@@ -24,11 +24,12 @@ function mtphr_shortcodes_admin_scripts( $hook ) {
 		wp_enqueue_style( 'mtphr-shortcodes-metaboxer' );
 	}
 
-	wp_register_style( 'brankic', MTPHR_SHORTCODES_URL.'/assets/css/brankic.css', false, MTPHR_SHORTCODES_VERSION );
-	wp_enqueue_style( 'brankic' );
+	wp_register_style( 'mtphr-shortcodes-font', MTPHR_SHORTCODES_URL.'/assets/fontastic/styles.css', false, MTPHR_SHORTCODES_VERSION );
+	wp_enqueue_style( 'mtphr-shortcodes-font' );
 
 	wp_register_style( 'mtphr-shortcodes', MTPHR_SHORTCODES_URL.'/assets/css/style-admin.css', false, MTPHR_SHORTCODES_VERSION );
 	wp_enqueue_style( 'mtphr-shortcodes' );
+	
 
 	// Shortcode generator
 	wp_register_script( 'mtphr-shortcodes-generator', MTPHR_SHORTCODES_URL.'/assets/js/shortcode-generator.js', array('jquery'), MTPHR_SHORTCODES_VERSION, true );
@@ -54,8 +55,8 @@ add_action( 'wp_enqueue_scripts', 'mtphr_shortcodes_scripts' );
  */
 function mtphr_shortcodes_scripts() {
 
-	wp_register_style( 'brankic', MTPHR_SHORTCODES_URL.'/assets/css/brankic.css', false, MTPHR_SHORTCODES_VERSION );
-	wp_enqueue_style( 'brankic' );
+	wp_register_style( 'mtphr-shortcodes-font', MTPHR_SHORTCODES_URL.'/assets/fontastic/styles.css', false, MTPHR_SHORTCODES_VERSION );
+	wp_enqueue_style( 'mtphr-shortcodes-font' );
 
 	wp_register_style( 'mtphr-shortcodes', MTPHR_SHORTCODES_URL.'/assets/css/style.css', false, MTPHR_SHORTCODES_VERSION );
 	wp_enqueue_style( 'mtphr-shortcodes' );
@@ -69,7 +70,7 @@ function mtphr_shortcodes_scripts() {
 	wp_register_script( 'jquery-easing', MTPHR_SHORTCODES_URL.'/assets/js/jquery.easing.1.3.js', array('jquery'), MTPHR_SHORTCODES_VERSION, true );
 	wp_register_script( 'mtphr-post-slider', MTPHR_SHORTCODES_URL.'/assets/js/mtphr-post-slider.js', array('jquery', 'jquery-easing'), MTPHR_SHORTCODES_VERSION, true );
 	wp_register_script( 'mtphr-slide-graph', MTPHR_SHORTCODES_URL.'/assets/js/mtphr-slide-graph.js', array('jquery', 'jquery-easing'), MTPHR_SHORTCODES_VERSION, true );
-	wp_register_script( 'mtphr-tabs', MTPHR_SHORTCODES_URL.'/assets/js/mtphr-tabs.js', array('jquery', 'jquery-easing'), MTPHR_SHORTCODES_VERSION, true );
+	//wp_register_script( 'mtphr-tabs', MTPHR_SHORTCODES_URL.'/assets/js/mtphr-tabs.js', array('jquery', 'jquery-easing'), MTPHR_SHORTCODES_VERSION, true );
 	wp_register_script( 'mtphr-toggles', MTPHR_SHORTCODES_URL.'/assets/js/mtphr-toggles.js', array('jquery', 'jquery-easing'), MTPHR_SHORTCODES_VERSION, true );
 }
 
@@ -84,7 +85,7 @@ add_action( 'wp_footer', 'mtphr_shortcodes_footer_scripts' );
  */
 function mtphr_shortcodes_footer_scripts() {
 
-	global $mtphr_post_slider, $mtphr_slide_graphs, $mtphr_tabs, $mtphr_toggles;
+	global $mtphr_post_slider, $mtphr_post_gallery, $mtphr_slide_graphs, $mtphr_tabs, $mtphr_toggles;
 	$settings = mtphr_shortcodes_settings();
 
 	if( $mtphr_post_slider ) {
@@ -98,7 +99,7 @@ function mtphr_shortcodes_footer_scripts() {
 			});
 		</script>
 		<?php
-	}
+	}	
 	if( $mtphr_slide_graphs ) {
 		wp_print_scripts( 'mtphr-slide-graph' );
 		?>
